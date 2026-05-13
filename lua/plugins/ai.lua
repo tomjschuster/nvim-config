727707cd3a6fe28c8c59d9228849a216764dd2b4
Tom Schuster <tomjschuster@gmail.com>
2026-05-13 11:47:56 -0400

    Upgrade treesitter, codecompanion cli commands

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -17,9 +17,31 @@ return {
         chat = {
           adapter = 'claude_code',
           --  adapter = 'gemini_cli',
+          keymaps = {
+            acp_options = {
+              modes = { n = 'gp' },
+              description = 'ACP session options (mode/reasoning)',
+              callback = function(chat)
+                require('codecompanion.interactions.chat.slash_commands.builtin.acp_session_options')
+                  .new({ Chat = chat, config = {}, context = {} })
+                  :execute()
+              end,
+            },
+          },
         },
         inline = {
           adapter = 'claude_proxy',
+        },
+        cli = {
+          agent = 'claude_code',
+          agents = {
+            claude_code = {
+              cmd = 'claude',
+              args = {},
+              description = 'Claude Code CLI',
+              provider = 'terminal',
+            },
+          },
         },
       },
       opts = {
@@ -39,11 +61,13 @@ return {
         },
       },
     },
+    cmd = { 'CodeCompanion', 'CodeCompanionChat', 'CodeCompanionActions', 'CodeCompanionCmd', 'CodeCompanionCLI' },
     keys = {
       { '<leader>aa', '<cmd>CodeCompanionActions<cr>', mode = { 'n', 'v' }, desc = '[A]I [A]ctions (Menu)' },
       { '<leader>ac', '<cmd>CodeCompanionChat Toggle<cr>', mode = { 'n', 'v' }, desc = '[A]I [C]hat Toggle' },
       { '<leader>an', '<cmd>CodeCompanionChat<cr>', mode = { 'n', 'v' }, desc = '[A]I [N]ew Chat' },
       { '<leader>ai', '<cmd>CodeCompanion<cr>', mode = { 'n', 'v' }, desc = '[A]I [I]nline Prompt' },
+      { '<leader>al', '<cmd>CodeCompanionCLI<cr>', mode = { 'n', 'v' }, desc = '[A]I C[L]I' },
       { 'ga', '<cmd>CodeCompanionChat Add<cr>', mode = 'v', desc = 'Add selection to AI Chat' },
     },
   },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,83 +1,94 @@
+local parsers = {
+  'bash',
+  'c',
+  'css',
+  'diff',
+  'html',
+  'lua',
+  'luadoc',
+  'markdown',
+  'markdown_inline',
+  'query',
+  'vim',
+  'vimdoc',
+  'elixir',
+  'javascript',
+  'typescript',
+  'tsx',
+  'json',
+  'yaml',
+}
+
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
+    branch = 'main',
     lazy = false,
     priority = 1100,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
-    dependencies = {
-      { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'master' },
-    },
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'css',
-        'diff',
-        'html',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-        'elixir',
-        'javascript',
-        'typescript',
-        'tsx',
-        'json',
-        'jsonc',
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<C-n>',
-          node_incremental = '<C-n>',
-          scope_incremental = '<C-s>',
-          node_decremental = '<C-p>',
-        },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-      textobjects = {
+    config = function()
+      require('nvim-treesitter').install(parsers)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local ft = args.match
+          local lang = vim.treesitter.language.get_lang(ft) or ft
+          if not lang or lang == '' then
+            return
+          end
+          local ok = pcall(vim.treesitter.start, args.buf, lang)
+          if ok then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    event = { 'BufReadPost', 'BufNewFile' },
+    config = function()
+      require('nvim-treesitter-textobjects').setup {
         select = {
-          enable = true,
           lookahead = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-            ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
-          },
         },
         move = {
-          enable = true,
           set_jumps = true,
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
         },
-      },
-    },
+      }
+
+      local select_textobject = require('nvim-treesitter-textobjects.select').select_textobject
+      local move = require 'nvim-treesitter-textobjects.move'
+
+      local selects = {
+        af = '@function.outer',
+        ['if'] = '@function.inner',
+        ac = '@class.outer',
+        ic = '@class.inner',
+      }
+      for lhs, capture in pairs(selects) do
+        vim.keymap.set({ 'x', 'o' }, lhs, function()
+          select_textobject(capture, 'textobjects')
+        end)
+      end
+
+      local moves = {
+        [']m'] = { 'goto_next_start', '@function.outer' },
+        [']M'] = { 'goto_next_end', '@function.outer' },
+        ['[m'] = { 'goto_previous_start', '@function.outer' },
+        ['[M'] = { 'goto_previous_end', '@function.outer' },
+        [']]'] = { 'goto_next_start', '@class.outer' },
+        [']['] = { 'goto_next_end', '@class.outer' },
+        ['[['] = { 'goto_previous_start', '@class.outer' },
+        ['[]'] = { 'goto_previous_end', '@class.outer' },
+      }
+      for lhs, spec in pairs(moves) do
+        vim.keymap.set({ 'n', 'x', 'o' }, lhs, function()
+          move[spec[1]](spec[2], 'textobjects')
+        end)
+      end
+    end,
   },
 }
